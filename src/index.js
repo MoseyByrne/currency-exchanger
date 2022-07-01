9e644e7d1dac9d2bf3eb606a566ad49466cdab74
MoseyByrne <mosey.b.byrne@gmail.com>
2022-07-01 12:03:39 -0700

    WIP html and js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currencyService.js';
 
+
+function clearFields() {
+  $('.showExchange').text()
+}
 
 $(document).ready(function() {
-
-})
+  $('#selectCurrency').submit(function(event) {
+    event.preventDefault();
+    const currency = $("input#currency").val();
+    clearFields();
+    CurrencyService.getExchange(currency)
+      .then(function(response) {
+        getExchange(response);
+      });
+  });
+});
